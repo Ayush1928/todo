39ab2ef8520aa97ae0d79bd5579b5dbc589b9d66
Ayush1928 <96259539+Ayush1928@users.noreply.github.com>
2023-09-10 13:07:26 +0530

    Commit 22

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import todoRouter from "./Routes/todoRoute.js"
-dotenv.config({path: './.env.local'});
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
